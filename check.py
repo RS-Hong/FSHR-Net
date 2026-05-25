560c19f34286083f333591c36e7a6d9719a5b32a
UltralyticsAssistant <web@ultralytics.com>
2026-05-25 05:21:57 +0000

    Auto-format by https://ultralytics.com/actions

--- check.py
+++ check.py
@@ -1,11 +1,10 @@
-from ultralytics import YOLO
-import ultralytics
 import torch
 
+from ultralytics import YOLO
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 model = YOLO(r"D:\code\ultralytics-SAR-V2\ultralytics\cfg\models\v8\yolov8s.yaml", verbose=True)
-model.model.to(device)   # 这句必须有
+model.model.to(device)  # 这句必须有
 
 x = torch.randn(1, 3, 640, 640, device=device)
 _ = model.model.predict(x, profile=True)
